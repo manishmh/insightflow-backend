2deea8fc3b42c3bd0e574981ca6eba113251f061
manishmh <manishmh982@gmail.com>
2024-06-05 10:58:28 +0530

    removed httponly from res cookie

--- routes/login.ts
+++ routes/login.ts
@@ -44,16 +44,14 @@ LoginRouter.post('/', async (req: Request, res: Response) => {
         
         // send acces token as cookie
         res.cookie('accessToken', accessToken, { 
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            // secure: process.env.NODE_ENV === 'production',
+            // sameSite: 'strict',
             maxAge: 86400000
 
         });
         res.cookie('refreshToken', refreshToken, { 
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            // secure: process.env.NODE_ENV === 'production',
+            // sameSite: 'strict',
             maxAge: 86400000
         });
         res.status(201).json({ 
